ef9a3d691cff4ebca4da3651561f49380136d315
Ahad-Dngwala <ahaddangarvawala@gmail.com>
2026-06-14 15:12:01 +0530

    Fix release URL tag to KNEMOSv1.0.0

--- WEBSITE/FRONTEND/next.config.ts
+++ WEBSITE/FRONTEND/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/downloads/KNEMOS-Setup.exe',
-        destination: 'https://github.com/Ahad-Dngwala/KnemOS/releases/download/v1.0.0/KNEMOS-Setup.exe',
+        destination: 'https://github.com/Ahad-Dngwala/KnemOS/releases/download/KNEMOSv1.0.0/KNEMOS-Setup.exe',
         permanent: false,
       },
     ];
